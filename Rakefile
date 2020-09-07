f536ab54cbc2f1b42f6de9ff8372fcc5afa0199c
Jon Adams <t12nslookup@gmail.com>
2020-09-07 12:12:22 +0100

    initial move to new rubygems structure

--- Rakefile
+++ Rakefile
@@ -1,6 +1,13 @@
 require "bundler/gem_tasks"
-require "rspec/core/rake_task"
+#require "rspec/core/rake_task"
+require "rake/extensiontask"
 
 #RSpec::Core::RakeTask.new(:spec)
 
 #task :default => :spec
+
+
+Rake::ExtensionTask.new("advantage") do |ext|
+  ext.lib_dir = "lib/advantage"
+end
+
--- lib/advantage/version.rb
+++ lib/advantage/version.rb
@@ -1,6 +1,6 @@
 module Advantage
   pkg_version = ''
-  File.open(File.join('ext', 'advantage.c') ) do |f|
+  File.open(File.join('ext', 'advantage', 'advantage.c') ) do |f|
     f.grep( /const char\* VERSION/ ) do |line|
       pkg_version = /\s*const char\* VERSION\s*=\s*["|']([^"']*)["|'];\s*/.match(line)[1]
     end
